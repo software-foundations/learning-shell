7dbf82d7824705c197b1d04a38b8a78005997566
bruno.silva5 <bruno.silva@sysdata.com.br>
2022-03-11 14:32:47 -0300

    coder -> [01_coder.sh, at /web_tutorials]

---
+++ web_tutorials/01_coder.sh
@@ -0,0 +1,29 @@
+##################################################################
+# tutorial: https://blog.cod3r.com.br/terminal-no-macos-e-linux/ #
+##################################################################
+
+# -> list all files
+ls -a
+
+# -> list
+ls -l
+
+# -> long ls
+ls -l
+
+# -> join flags
+ls -al
+
+# -> comand k: cleans the terminal
+
+# -> change directory
+# cd <path>
+
+# -> creating folders
+# mkdir <folder name>
+
+# -> removing folders
+# rmdir <folder name>
+
+# -> check version of a software
+# <software> --version
